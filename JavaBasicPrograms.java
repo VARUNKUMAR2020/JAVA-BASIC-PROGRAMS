f589ddff1f5c3c2bcacfc19df6d6a6d075ad4c83
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-11 12:56:03 +0530

    ADD:Check string is equal or not

--- JavaBasicPrograms.java
+++ JavaBasicPrograms.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 public class JavaBasicPrograms {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("To check string equal or not: ");
+
+            // To Check Two String Equal Or Not
+            Scanner a = new Scanner(System.in);
+            System.out.println("Enter the First String: ");
+            String s1 = a.nextLine();
+            System.out.println("Enter the First String: ");
+            String s2 = a.nextLine();
+            if (s1.compareTo(s2) == 0) {
+                System.out.println("Given String Are Equal");
+            } else {
+                System.err.println("Given String Are Not Equal");
+            }
     }
 }
