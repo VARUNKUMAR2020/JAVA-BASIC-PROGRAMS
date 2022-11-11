719129e2aa8d6c1c5b94b2e3b580848b78eb6ee7
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-11 13:06:15 +0530

    ADD:To sum and count invalid entries

--- JavaBasicPrograms.java
+++ JavaBasicPrograms.java
@@ -1,5 +1,15 @@
 public class JavaBasicPrograms {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < args.length; i++) {
+            try {
+                sum += Integer.parseInt(args[i]);
+            } catch (NumberFormatException e) {
+                count++;
+            }
+        }
+        System.out.println("Sum = " + sum);
+        System.out.println("Number of invalid integers = " + count);
     }
 }
