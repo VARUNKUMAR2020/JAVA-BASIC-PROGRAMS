30466bad0287e4b88d24def70f2879c1f091b0d5
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-11 13:36:06 +0530

    ADD: Static variables,block and method

--- JavaBasicPrograms.java
+++ JavaBasicPrograms.java
@@ -1,5 +1,25 @@
 public class JavaBasicPrograms {
+    // a static variable
+    static int a = 10;
+
+    // a static method
+    static void method() {
+        System.out.println("From static method");
+    }
+
+    // a static block
+    static {
+        System.out.println("From static block");
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        // calling a static method
+        // without creating an object
+        method();
+
+        // accessing a static variable
+        // without creating an object
+        System.out.println(a);
     }
 }
