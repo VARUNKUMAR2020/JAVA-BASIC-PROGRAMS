f37468bd16dced6c76bc2a10724f0eed8b6a0fa0
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-11 12:51:56 +0530

    ADD:Default values of primitive datatypes

--- JavaBasicPrograms.java
+++ JavaBasicPrograms.java
@@ -1,5 +1,28 @@
 public class JavaBasicPrograms {
+    // Primitive Data Types
+    boolean b;
+    char c;
+    byte by;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Default values of Primitive DataTypes: ");
+
+        // Creating A Object Of The Class
+        JavaBasicPrograms demo = new JavaBasicPrograms();
+
+        // Displaying All Primitive Data Types
+        System.out.println("Default value of Boolean  : " + demo.b);
+        System.out.println("Default value of Character: " + demo.c);
+        System.out.println("Default value of Byte     : " + demo.by);
+        System.out.println("Default value of Short    : " + demo.s);
+        System.out.println("Default value of Int      : " + demo.i);
+        System.out.println("Default value of Long     : " + demo.l);
+        System.out.println("Default value of Float    : " + demo.f);
+        System.out.println("Default value of Double   : " + demo.d);
     }
 }
