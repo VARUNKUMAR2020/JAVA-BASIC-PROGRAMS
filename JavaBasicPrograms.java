83c77a85031885194f0e74de6d76644a5b29182a
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-11 12:45:51 +0530

    ADD:Display of message

--- JavaBasicPrograms.java
+++ JavaBasicPrograms.java
@@ -1,5 +1,5 @@
 public class JavaBasicPrograms {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java Programs :) ");
     }
 }
